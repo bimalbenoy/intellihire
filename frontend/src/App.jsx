3f36938af03145d313df99f1eb1f93cc45a9a2aa
Abhijith <abhijithgangadharan.mec@gmail.com>
2025-07-31 14:02:24 +0530

    fix vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import Candidate from './pages/candidate'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';  
+import SignUp from './components/auth/SignUp';
+import Login from './components/auth/Login';
 function App() {
   return (
     <>
-      <Candidate />
+      <Router>
+      <Routes>
+        <Route path="/candidate" element={<Candidate />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
     </>
   )
 }
